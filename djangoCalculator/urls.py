9aa5e9b81469c93713dbb1f085eb4053f0b30fcf
Musfiq <mushfiqur15-10872@diu.edu.bd>
2022-03-22 05:25:42 +0600

    Done

--- djangoCalculator/urls.py
+++ djangoCalculator/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path('', include('rootapp.urls')),
+    path('root', include('rootapp.urls')),
     path('admin/', admin.site.urls),
-    path('calculatorapp', include('calculatorapp.urls'))
+    path('', include('calculatorapp.urls'))
 ]
