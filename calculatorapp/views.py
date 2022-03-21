7ce9e95045e6417b58bc98890253887fafc01eac
Musfiq <mushfiqur15-10872@diu.edu.bd>
2022-03-22 03:21:43 +0600

    query submit and add instructions

--- calculatorapp/views.py
+++ calculatorapp/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 
 # Create your views here.
 
 def index(request):
    return render(request, 'index.html')
+
+def submitquery(request):
+   q = request.GET['query']
+   return HttpResponse(q)
