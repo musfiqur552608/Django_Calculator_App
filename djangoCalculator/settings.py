42fc44bc2cbf9c70108c01aedac53980133c233d
Musfiq <mushfiqur15-10872@diu.edu.bd>
2022-03-22 02:24:31 +0600

    Make navigation and input area

--- djangoCalculator/settings.py
+++ djangoCalculator/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'calculatorapp'
+    'calculatorapp',
+    'rootapp'
 ]
 
 MIDDLEWARE = [
@@ -56,7 +57,7 @@ ROOT_URLCONF = 'djangoCalculator.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'templates')],
+        'DIRS': [os.path.join(BASE_DIR,'templates/calculatorapp')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- rootapp/views.py
+++ rootapp/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def index(request):
-   return render(request, 'index.html')
+def root(request):
+   return HttpResponse("Server Started")
